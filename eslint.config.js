5502fd147740baa67ae11ce737de255ec413d41f
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-04-02 11:52:54 +0200

    build: remove eslint plugins, add rules
    #1

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,17 @@
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import js from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import nPlugin from 'eslint-plugin-n';
-import promisePlugin from 'eslint-plugin-promise';
-import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-  env: {
-    browser: true,
-    es2021: true, 
-  },
-  extends: [
-    'eslint:recommended',            
-    'plugin:import/recommended',     
-    'plugin:n/recommended',          
-    'plugin:promise/recommended',    
-    'plugin:prettier/recommended',   
-  ],
-  
-  parserOptions: {
-    ecmaVersion: 12,     
-    sourceType: 'module', 
-  },
-  
-  plugins: [
-    'import',        // Import plugin for managing import/export rules
-    'n',             // Node.js plugin (optional)
-    'promise',       // Promise plugin for async operations
-    'prettier',      // Prettier plugin to enforce formatting rules
-  ],
-
-  rules: {
-    // Prettier formatting options 
-    'prettier/prettier': [
-      'error', 
-      {
-        singleQuote: true,      
-        semi: true,             
-        trailingComma: 'es5',   
-      },
-    ],
-    // Warn if `console` is used in production
-    'no-console': 'warn',
-    // Warn if there are unused variables, but allow unused function arguments with underscores
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    // Disabling the rule for unresolved imports
-    'import/no-unresolved': 'off',
-  },
-});
+export default defineConfig([
+	{        
+        rules: {
+        // Basic code style rules
+        "semi": ["error", "always"],  // Always require semicolons
+        "quotes": ["error", "single"],  // Enforce single quotes for strings
+        "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],  // Warn on unused variables except those starting with "_"
+        "indent": ["error", 2],  // Enforce 2 spaces for indentation
+        "linebreak-style": ["error", "unix"],  // Enforce UNIX linebreaks (\n)
+        "no-console": "warn",  // Warn on console usage
+        "no-debugger": "warn",  // Warn on debugger usage
+        "eqeqeq": ["error", "always"],  // Enforce strict equality (===)
+        },
+	},
+]);
