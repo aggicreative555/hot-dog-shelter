eecf96a89b69723b567a024b353e1548408f0f66
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-04-10 12:07:46 +0200

    feat(global): add logout button logic
    #35 #19

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -4,16 +4,19 @@ import { goBackButton } from "../../ui/global/goBackButton";
 import { shareButton } from "../../ui/global/shareButton";
 import { profileTemplate } from "../../ui/profile/profileTemplate";
 import { setupPostClickNavigation } from "../../ui/post/renderPost";
+import { setLogoutListener } from "../../ui/global/logout";
 
 function initializeProfile() {
   authGuard();
   const container = document.getElementById("postsContainer");
+  const profileContainer = document.getElementById("profileContainer");
 
   if (container) {
     profileTemplate(container);
     setupPostClickNavigation();
     goBackButton();
     shareButton();
+    setLogoutListener(profileContainer);
     readPostsByOwner(container);
   } else {
     console.error("No element with id #postsContainer found in the DOM.");
--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,42 @@
-export function setLogoutListener() {}
+import { remove } from "../../utilities/authGuard";
+
+/**
+ * Attaches a logout button to the given container.
+ *
+ * On click, the function:
+ * - Clears user-related data from localStorage and sessionStorage.
+ * - Displays a confirmation message in #userSuccess.
+ * - Redirects the user to the home page after a short delay.
+ *
+ * @function setLogoutListener
+ * @param {HTMLElement} container - The DOM element where the logout button will be appended.
+ */
+
+export function setLogoutListener(container) {
+  const logoutButton = document.createElement("button");
+  const messageContainer = document.getElementById("userSuccess");
+
+  logoutButton.innerHTML = "Log out";
+
+  logoutButton.addEventListener("click", () => {
+    try {
+      localStorage.clear();
+      sessionStorage.clear();
+
+      if (messageContainer) {
+        messageContainer.innerHTML =
+          "You are now logged out. Redirecting to home page...";
+      } else {
+        console.error("#userSuccess not found in DOM.");
+      }
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 1000);
+    } catch (error) {
+      console.error("Logout unsuccessful.", error.message);
+      throw error;
+    }
+  });
+
+  container.prepend(logoutButton);
+}
